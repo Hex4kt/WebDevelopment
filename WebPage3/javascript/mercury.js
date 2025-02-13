af379231ed6e41514ca04ebfc13448dfb06d54e1
Hex.Official <jan-heiner@web.de>
2025-02-13 19:02:02 +0100

    update

---
+++ WebPage3/javascript/mercury.js
@@ -0,0 +1,10 @@
+
+function buttonClick() {
+    
+    var name = "Mercury";
+    var age = "alter Opa";
+
+    var message = "Hi, my name is " + name + " and I am ein " + age;
+
+    alert(message);   
+}
